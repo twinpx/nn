aeaf035dbd1df45bac385c04d05f1d816a6680a3
djembuka <djembuka@gmail.com>
2022-08-03 13:20:21 +0300

    Filter changes

--- source/js/main.js
+++ source/js/main.js
@@ -11,6 +11,14 @@ if ($('.b-filter').length) {
   $('.b-filter .col.xl9').niceScroll();
 }
 
+//placeholder
+setTimeout(function () {
+  $(
+    '.b-header, h1, .b-breadcrumbs, #gallery, #planText, #buildings, .b-heading, #detail, #plan, #prices, #visual, #actions, #actionsDetail, #news, #newsDetail, #bigGallery, #docs, #hypothec, #flat-choice'
+  ).addClass('i-ph-animated');
+}, 500);
+
+//sidenav
 $('.sidenav').sidenav({
   edge: 'right',
 });
@@ -20,33 +28,13 @@ $('.b-sidenav-close').click(function (e) {
   $('.sidenav').sidenav('close');
 });
 
-//placeholder
-setTimeout(function () {
-  $(
-    '.b-header, h1, .b-breadcrumbs, #gallery, #planText, #buildings, .b-heading, #detail, #plan, #prices, #visual, #actions, #actionsDetail, #news, #newsDetail, #bigGallery, #docs, #hypothec, #flat-choice'
-  ).addClass('i-ph-animated');
-}, 500);
-
-//scroll effects
-/*$( window ).scroll( function() {
-  
-  //choose a flat
-  if ( document.getElementById( 'flat-choice' ) && ($( window ).scrollTop() + $( window ).height() - 200) > $( '#flat-choice' ).offset().top ) {
-    $( '#flat-choice' ).addClass( 'i-scroll-animated' );
-  }
-  
-  //advantages
-  if ( document.getElementById( 'advantages' ) && ($( window ).scrollTop() + $( window ).height() - 200) > $( '#advantages' ).offset().top ) {
-    $( '#advantages' ).addClass( 'i-scroll-animated' );
-  }
-  
-  //description
-  if ( document.getElementById( 'description' ) && ($( window ).scrollTop() + $( window ).height() - 200) > $( '#description' ).offset().top ) {
-    $( '#description' ).addClass( 'i-scroll-animated' );
-  }
-  
-  //plan
-  if ( document.getElementById( 'plan' ) && ($( window ).scrollTop() + $( window ).height() - 200) > $( '#plan' ).offset().top ) {
-    $( '#plan' ).addClass( 'i-scroll-animated' );
-  }
-}).scroll();*/
+document
+  .querySelectorAll('#sideNav .b-sidenav-sub__title')
+  .forEach(function (sub) {
+    sub.addEventListener('click', function (e) {
+      e.preventDefault();
+      e.target.parentNode
+        .querySelector('.b-sidenav-sub__menu')
+        .classList.toggle('i-open');
+    });
+  });
